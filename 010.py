ed2835da1a23ae7215597bad4a9413b359cfd28f
chinococo <garyliuyahoocom384@gmail.com>
2021-10-06 12:21:45 +0800

    update

--- 010.py
+++ 010.py
@@ -12,19 +12,19 @@ dic = {'A': 1
     , 'Q': 0.5
     , 'K': 0.5
        }
-a=0
-b=0
+a = 0
+b = 0
 for i in range(3):
-    a+=dic[str(input())]
+    a += dic[str(input())]
 for i in range(3):
-    b+=dic[str(input())]
-if(a>10.5):
+    b += dic[str(input())]
+if (a > 10.5):
     a = 0
 if (b > 10.5):
     b = 0
-if(abs(10.5-a)==abs(10.5-b)):
-    print('Tie',end='')
-elif (abs(10.5-a)>abs(10.5-b)):
-    print('B Win',end='')
+if (abs(10.5 - a) == abs(10.5 - b)):
+    print('Tie', end='')
+elif (abs(10.5 - a) > abs(10.5 - b)):
+    print('B Win', end='')
 else:
-    print('A Win',end='')
+    print('A Win', end='')
--- quiz.py
+++ quiz.py
@@ -1,11 +1,11 @@
 card = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 score =[1,2,3,4,5,6,7,8,9,10,0.5,0.5,0.5]
+def sc():
+    return score[card.index(str(input()))]+score[card.index(str(input()))]+score[card.index(str(input()))]
 a=0.0
 b=0.0
-for i in range(3):
-    a+=score[card.index(str(input()))]
-for i in range(3):
-    b+=score[card.index(str(input()))]
+a=sc()
+b=sc()
 if(a>10.5):
     a = 0
 if (b > 10.5):
