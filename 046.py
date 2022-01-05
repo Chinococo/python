70fcc3d7261ac31a59b8dfd1e051274a23291fa7
chinococo <garyliuyahoocom384@gmail.com>
2022-01-05 17:35:32 +0800

    update

--- 046.py
+++ 046.py
@@ -1,5 +1,29 @@
+list1 = [0]
+def dfs(data,sum,index,queue,prev,ban,d,maxd):
+    if index>=len(data) :
+        if d!=maxd:
+            dfs(data,sum,0,[],0,ban.copy().union(queue),d+1,maxd)
+        else:
+            #print(ban)
+            list1.append(sum)
+        return
+
+    if prev<=data[index][0] and not index in ban:
+        t1 = queue.copy()
+        t1.append(index)
+        dfs(data,sum+data[index][1]-data[index][0],index+1,t1,data[index][1],ban,d,maxd)
+    t1 = queue.copy()
+    dfs(data,sum,index+1,t1,prev,ban,d,maxd)
 def main():
 
-    pass
+    n,m = map(int,input().split())
+    data = []
+    for i in range(m):
+        index,f,e = map(int,input().split())
+        data.append((f,e,))
+    data.sort()
+    dfs(data,0,0,[],-1,set(),1,n)
+    list1.sort()
+    print(list1[-1])
 if __name__ == '__main__':
     main()
